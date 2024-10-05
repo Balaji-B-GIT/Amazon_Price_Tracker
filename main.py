55cf298e2532287e5b6222d17e7d5e12198a63ac
Balaji B <balaji.balachandar1@gmail.com>
2024-10-05 12:59:27 +0530

    Final Commit

--- main.py
+++ main.py
@@ -7,22 +7,30 @@ from bs4 import BeautifulSoup
 load_dotenv("C:/Python/Environmental variables/.env")
 my_mail = "sampleforpythonmail@gmail.com"
 password = os.getenv("smtp_app_password")
-URL = "https://appbrewery.github.io/instant_pot/"
+URL = "https://www.amazon.in/dp/B0CGCZTYH2/ref=twister_B0CVQ9K5ZL?_encoding=UTF8&th=1"
+headers = {
+    "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/85.0.4183.102 Safari/537.36",
+    "Accept-Language": "en-US,en"
+}
 
-res = requests.get(URL)
+res = requests.get(URL,headers=headers)
 data = res.content
 
 soup = BeautifulSoup(data,"html.parser")
 
-price = soup.find(class_="aok-offscreen").getText()
-modified_price = float(price.split("$")[-1])
+# price = soup.find(class_="a-price-whole")
+# print(price.getText())
+
+price_data = soup.find(class_="aok-offscreen").getText()
+indian_price = price_data.split(" ")[3]
+modified_price = float(indian_price.split("₹")[1].replace(",",""))
 
 title = soup.find(id="productTitle",class_ = "a-size-large product-title-word-break").get_text()
-formatted_title = ' '.join(title.split()).split(",")[0]
+formatted_title = ' '.join(title.split())
 
-body = f"{formatted_title} is now ${modified_price}"
+body = f"{formatted_title} is now available for INR {modified_price}\nBuy link: {URL}"
 
-if modified_price < 100.00:
+if modified_price < 3000.00:
     with smtplib.SMTP("smtp.gmail.com", 587) as connection:
         connection.starttls()
         connection.login(my_mail, password=password)
@@ -30,3 +38,6 @@ if modified_price < 100.00:
                             to_addrs=my_mail,
                             msg=f"Subject:Price alert!!!\n\n{body}")
         print("mail sent successfully")
+
+
+# We can make this code run daily at a specific time using PYTHONANYWHERE...
